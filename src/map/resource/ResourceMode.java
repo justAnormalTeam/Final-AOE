c2496bbb87b99a17e9b6f0d2efcfe23f7e3104b1
SpiritWeaver <erfan.noohi@gmail.com>
2017-07-17 01:33:14 +0430

    made a new enum ... resource modes now maybe we should have a player class or something to have and instance of resources and the units each player has ...

---
+++ src/map/resource/ResourceMode.java
@@ -0,0 +1,11 @@
+package map.resource;
+
+/**
+ * Created by Asus on 7/17/2017.
+ */
+public enum  ResourceMode {
+    gold,
+    silver,
+    meat,
+    lumber
+}
--- src/map/resource/ResourceType.java
+++ src/map/resource/ResourceType.java
@@ -10,20 +10,21 @@ import java.util.Random;
  */
 public enum ResourceType
 {
-    TREE(new Color(34,139,34), TerrainType.GRASS, 2, ".png", 1000),
-    PALM(new Color(51,25,0), TerrainType.SAND, 1, ".png", 30),
-    DEEP_FISH(new Color(255,0,255), TerrainType.DEEP_WATER, 1, ".gif", 100),
-    SHALLOW_FISH(new Color(255,102,102), TerrainType.SHALLOW_WATER, 1, ".gif", 20),
-    GOLD(new Color(255,255,0), TerrainType.PEAK, 1, ".png", 100),
-    SILVER(new Color(160,160,160), TerrainType.MOUNTAIN, 1, ".png", 100);
+    TREE(new Color(34,139,34), TerrainType.GRASS, 2, ".png", 10000, ResourceMode.lumber),
+    PALM(new Color(51,25,0), TerrainType.SAND, 1, ".png", 3000, ResourceMode.lumber),
+    DEEP_FISH(new Color(255,0,255), TerrainType.DEEP_WATER, 1, ".gif", 10000, ResourceMode.meat),
+    SHALLOW_FISH(new Color(255,102,102), TerrainType.SHALLOW_WATER, 1, ".gif", 20000, ResourceMode.meat),
+    GOLD(new Color(255,255,0), TerrainType.PEAK, 1, ".png", 100000, ResourceMode.gold),
+    SILVER(new Color(160,160,160), TerrainType.MOUNTAIN, 1, ".png", 100000, ResourceMode.silver);
 
     private Color color;
     private TerrainType availableTerrain;
     private int differentType;
     private String extension;
     private int amount;
+    private ResourceMode mode;
 
-    ResourceType(Color color, TerrainType terrainType, int differentType, String extension, int amount)
+    ResourceType(Color color, TerrainType terrainType, int differentType, String extension, int amount, ResourceMode mode)
     {
         this.color = color;
         this.availableTerrain = terrainType;
@@ -32,8 +33,15 @@ public enum ResourceType
         Random random = new Random(System.currentTimeMillis());
         amount= (int)( (random.nextDouble()+1) * amount );
         this.amount = amount;
+        this.mode = mode;
     }
 
+    public void gatherResource(int decreaseAmount){
+        // we should pass something to this method and add the gathered resource to that player.
+        amount -= decreaseAmount;
+    }
+
+    // getters and setters:
     public int getDifferentType()
     {
         return differentType;
